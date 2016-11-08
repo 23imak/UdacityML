6031594151bedaaa0c12c149127b1e32e4e42a51
Asif Khan <23imak@gmail.com>
2016-11-08 23:09:45 +0530

    Exploring Enron Data Set

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,65 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print len(enron_data)
+print set(len(v) for v in enron_data.values())
+
+#length of poi
+numPois = []
+for x in enron_data.values():
+    if x['poi'] == True:
+        numPois.append(x['poi'])
+print "Number of POI : ", len(numPois)
+
+#total stock value of James Prentice
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+#total mails send from Wesley Colwell to poi
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+#total exercised stock options of Jefferey Skilling
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+#total payment of poi
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+
+#no of people with quantified salary
+salary_array = []
+for x in enron_data.values():
+    if x['salary'] == 'NaN':
+        salary_array.append(x['salary'])
+
+print len(enron_data) - len(salary_array)
+
+#no of people with email address
+email_array = []
+for x in enron_data.values():
+    if x['email_address'] == 'NaN':
+        email_array.append(x['email_address'])
+print len(enron_data) - len(email_array)
+
+#percentage of people havong NaN in thier total payments
+payment_array = []
+for x in enron_data.values():
+    if x['total_payments'] == 'NaN':
+        payment_array.append(x['total_payments'])
+print len(payment_array) #/146
+
+
+# percentege of poi in dataset having NaN for thier total payments
+enron_data_poi_array = []
+
+for x in enron_data.values():
+    if x['poi'] == True:
+        enron_data_poi_array.append(x)
+
+enron_data_poi_NaNpayments = []
+
+for x in enron_data_poi_array:
+    if x['total_payments'] == 'NaN':
+        enron_data_poi_NaNpayments.append(x['total_payments'])
+
+print len(enron_data_poi_NaNpayments)
 
