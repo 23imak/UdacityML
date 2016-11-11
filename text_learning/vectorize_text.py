bd740e81a7b977963357022be6ddcc995e7fcfb4
Asif Khan <23imak@gmail.com>
2016-11-11 17:08:44 +0530

    text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -34,30 +34,38 @@ word_data = []
 ### can take a long time
 ### temp_counter helps you only look at the first 200 emails in the list so you
 ### can iterate your modifications quicker
-temp_counter = 0
+#temp_counter = 0
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+        #temp_counter += 1
+        #if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text =  parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            for signature_word in ["sara", "shackleton", "chris", "germani"]:
+                text = text.replace(signature_word,"")
 
             ### append the text to word_data
+            word_data.append(text)
+
+
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(0 if name == "sara" else 1)
 
             email.close()
+
+
+print "Entry #152: ", word_data[152]
 
 print "emails processed"
 from_sara.close()
@@ -72,4 +80,13 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 ### in Part 4, do TfIdf vectorization here
 
+from nltk.corpus import stopwords
+sw = stopwords.words("english")
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english",lowercase=True)
+vectorizer.fit_transform(word_data)
+
+print len(vectorizer.get_feature_names())
+
+print vectorizer.get_feature_names()[34597]
