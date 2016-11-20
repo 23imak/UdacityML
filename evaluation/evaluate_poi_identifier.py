557f7e09725d7dada38a4791aadaf356572e4642
Asif Khan <23imak@gmail.com>
2016-11-20 23:04:40 +0530

    Evalutaion using Precision and Recall

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,6 +13,11 @@
 
 import pickle
 import sys
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -27,5 +32,41 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+
+# Split data into training and testing
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, random_state=42, test_size=0.3)
+
+# Fit data with sklearn decision trees algorithm
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+# Get the accuracy
+from sklearn.metrics import accuracy_score
+prediction = clf.predict(features_test)
+
+
+
+print accuracy_score(prediction, labels_test)
+
+print "Number of poi's in test set", sum(prediction)
+print "Number of people in test set", len(features_test)
+print "Precison: ", precision_score(prediction,labels_test)
+print "Recall: ", recall_score(prediction,labels_test)
+
+print "Actual predcitions", prediction
+print "true test labels", labels_test
+
+prediction[4] = 0
+prediction[11] = 0
+prediction[19] = 0
+prediction[21] = 0
+
+print prediction
+
+print "Accuracy if prediction is 0 : ", accuracy_score(prediction,labels_test)
+
+
 
 
